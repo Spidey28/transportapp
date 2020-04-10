bcb0d37e91e97a16e09b8ba387e413b6aa5fe69d
spidey <mohit.sood@renewbuy.com>
2020-04-10 05:49:15 +0530

    Revised ajax request for experiment

--- dtc/urls.py
+++ dtc/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from dtc import views
 
 urlpatterns = [
-    path('', views.hello_world, name='hello_world'),
+    path('demo/', views.hello_world, name='hello_world'),
+    path('', views.welcome, name='hello_world'),
     path('maps/', views.maps, name='maps'),
+    path('routes/', views.routes, name='routes'),
 ]
--- dtc/views.py
+++ dtc/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from dtc.models import Route
+from django.http.response import JsonResponse
+from django.http import HttpResponse
+
+
 
 def hello_world(request):
 
@@ -13,3 +18,25 @@ def maps(request):
 def welcome(request):
 
     return render(request, 'welcome.html', {})
+
+
+def routes(request):
+
+    request_data = request.GET
+    route_id = request_data.get('id')
+    routes = Route.objects.get(id=route_id)
+    routes_all = Route.objects.all()
+    response = {
+        'source': routes.source,
+        'destination': routes.destination,
+        'routes': []
+    }
+    for routee in routes_all:
+        response.get('routes').append(
+            {
+                routee.id: routee.route_no
+            }
+        )
+    # return render(request, 'routes.html', {'routes': routes})
+    return JsonResponse(response)
+    # return HttpResponse(response, content_type='application/json')
